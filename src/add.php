2f73e8261e7b1680c00b2a72c3e9c7da647f69a8
Aleksey_Tsy <AlekseyTsymbalov@yahoo.com>
2025-04-30 18:14:19 +0300

    Убрал лишний отступ

--- src/add.php
+++ src/add.php
@@ -1,24 +1,23 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 require 'helper.php';
 
-    $task = $_POST["task"];
+$task = $_POST["task"];
 
-    if($_SERVER["REQUEST_METHOD"] === "POST"){
-        $task = $_POST["task"] ?? '';
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $task = $_POST["task"] ?? '';
 
-        if(empty(trim($task))){
-            die("Ошибка: поле задачи не может быть пустым!");
-        }
+    if (empty(trim($task))) {
+        die("Ошибка: поле задачи не может быть пустым!");
     }
-
+}
 
 $pdo = getPDO();
-    $psql = 'INSERT INTO work_list(task) VALUES(:task)';
-    $query = $pdo->prepare($psql);
-    $query->execute(['task' => $task ]);
+$psql = 'INSERT INTO work_list(task) VALUES(:task)';
+$query = $pdo->prepare($psql);
+$query->execute(['task' => $task]);
 
-    header('Location: /');
-    exit;
+header('Location: /');
+exit;
